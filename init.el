235d7710276df7cfc5b84167bc252478a130e79a
Richard Todd <rwtodd@users.noreply.github.com>
2021-01-17 15:13:17 -0600

    tweaked the font-selection code

--- init.el
+++ init.el
@@ -12,12 +12,11 @@
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
    ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"])
- '(custom-enabled-themes (quote (dracula)))
+ '(custom-enabled-themes '(dracula))
  '(custom-safe-themes
-   (quote
-    ("dcdd1471fde79899ae47152d090e3551b889edf4b46f00df36d653adc2bf550d" "68d8ceaedfb6bdd2909f34b8b51ceb96d7a43f25310a55c701811f427e9de3a3" default)))
+   '("2dff5f0b44a9e6c8644b2159414af72261e38686072e063aa66ee98a2faecf0e" "dcdd1471fde79899ae47152d090e3551b889edf4b46f00df36d653adc2bf550d" "68d8ceaedfb6bdd2909f34b8b51ceb96d7a43f25310a55c701811f427e9de3a3" default))
  '(hl-sexp-background-color "#efebe9")
- '(package-selected-packages (quote (slime dracula-theme edit-indirect markdown-mode))))
+ '(package-selected-packages '(slime dracula-theme edit-indirect markdown-mode)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -27,19 +26,19 @@
  )
 
 ;; load my custom codez...
-(add-to-list 'load-path "~/.emacs.d/rwt-lisp")
+(add-to-list 'load-path "~/.config/emacs/rwt-lisp")
 (require 'rwt-utils)
 (require 'rwt-blogging)
-(setq rwt/blog-base "~/src/ghio_pages")
+(setq rwt/blog-base "~/ghio-pages")
 
 ;; figure out a good font based on the ones installed... go for 11pt
 ;; font by default.
-(let ((ffs (font-family-list)))
-  (cond
-   ((member "Cascadia Mono" ffs) (set-frame-font "Cascadia Mono"))
-   ((member "Go Mono" ffs) (set-frame-font "Go Mono"))
-   ((member "Source Code Pro" ffs) (set-frame-font "Source Code Pro"))
-   ((member "Consolas" ffs) (set-frame-font "Consolas"))))
+(when-let ((available-font (car (cl-intersection
+				 '("Cascadia Mono" "Go Mono" "Source Code Pro" "Consolas")
+				 (font-family-list)
+				 :test #'string-equal))))
+  (set-face-attribute 'default nil :family available-font))
+;; generically set the font height, regardless of what we find, font-wise:
 (set-face-attribute 'default nil :height 110)
 
 ;; I tend to use sbcl
